1bb1567f38a85b158bbff4e0c94f5225851afb76
singhcodes25 <singhcodes07@gmail.com>
2026-05-15 23:52:52 +0530

    first commit

---
+++ src/app/admin/login/page.js
@@ -0,0 +1,42 @@
+'use client';
+import { signIn } from "next-auth/react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+    const router = useRouter();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const res = await signIn("credentials", {
+      redirect: false,
+      email,
+      password,
+    });
+    if (res?.error) alert("Invalid credentials");
+    else router.push("/admin");
+  };
+
+  return (
+    <form onSubmit={handleLogin} className="max-w-md mx-auto mt-20 min-h-96 p-6 border rounded">
+      <h1 className="text-2xl font-bold mb-4">Admin Login</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        className="w-full mb-3 p-2 border rounded"
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        className="w-full mb-3 p-2 border rounded"
+      />
+      <button type="submit" className="bg-green-600 text-white px-4 py-2 rounded">Login</button>
+    </form>
+  );
+}
